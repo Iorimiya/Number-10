89df31bc8c3a7d1d411f2342a7d8ed615d45b81c
Siorimiya <yukimi27043816@gmail.com>
2022-05-19 19:56:54 +0800

    update music player and unused code

--- app/src/main/java/ntou/cs/java2020/classproject/GlobalSettings.java
+++ app/src/main/java/ntou/cs/java2020/classproject/GlobalSettings.java
@@ -6,61 +6,105 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public abstract class GlobalSettings extends AppCompatActivity {
 
-    protected static boolean musicControl =true, skipControl =false;
-//    musicControl：音樂控制選項
+    protected static boolean musicControl = true, skipControl = false;
+    //    musicControl：音樂控制選項
 //    skipControl：跳關控制選項
-    protected static int lastOpenedLevel=0;
-//    關卡控制選項，可以遊玩的最後一項關卡
+    protected static int lastOpenedLevel = 0;
+    //    關卡控制選項，可以遊玩的最後一項關卡
 //    opened level control option, the last level which the player can play
     private static final int totalLevel = 5;
-//    總關卡數
+    //    總關卡數
 //    total levels
-    protected static final ArrayList<Integer> scoreList=new ArrayList<>(totalLevel);
-//    計分表
-    protected static MediaPlayer mediaPlayer;
+    protected static final ArrayList<Integer> scoreList = new ArrayList<>(totalLevel);
+    //    計分表
+    protected static MediaPlayer musicPlayer;
 //    音樂播放器物件
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);}
-    protected boolean initializeMediaPlayer(){
-        if(GlobalSettings.mediaPlayer==null)
-            GlobalSettings.mediaPlayer = MediaPlayer.create(this, R.raw.song_of_the_ancients_devola);
-//            新增播放器
-//            create music player
-        if( GlobalSettings.mediaPlayer == null) return false;
-        GlobalSettings.mediaPlayer.setLooping(true);
-//            設定迴圈撥放
-//            set the single media loop play
-        GlobalSettings.mediaPlayer.setOnErrorListener((mp, what, extra) -> {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GlobalSettings.lastOpenedLevel = getSharedPreferences("NumberTenSaveData", MODE_PRIVATE).getInt("hasOpenedLevel", 0);
+        GlobalSettings.musicControl = getSharedPreferences("NumberTenSaveData", MODE_PRIVATE).getBoolean("musicControl", true);
+        if (GlobalSettings.scoreList.size() == 0)
+            for (int counter = 0; counter < 5; counter++)
+                GlobalSettings.scoreList.add(getSharedPreferences("NumberTenSaveData", MODE_PRIVATE).getInt(String.format(Locale.getDefault(), "level%dHighestScore", counter + 1), 0));
+//        從Shared Preferences取得相關資料
+//        get Data from Shared References
+        if (GlobalSettings.musicPlayer == null) {
+            createMusic(R.raw.song_of_the_ancients_devola, GlobalSettings.musicControl);
+        } else {
+            GlobalSettings.playMusic(GlobalSettings.musicControl);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GlobalSettings.playMusic(GlobalSettings.musicControl);
+//        依音樂控制選項決定是否撥放
+//        start the music player if the music control option is on.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        GlobalSettings.pauseMusic();
+    }
+
+    @Override
+    protected void onDestroy() {
+        GlobalSettings.playMusic(GlobalSettings.musicControl);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(GlobalSettings.this, Title.class));
+        finish();
+    }
+
+    private void createMusic(int song, boolean playControl) {
+        GlobalSettings.musicPlayer = MediaPlayer.create(this, song);
+        GlobalSettings.musicPlayer.setLooping(true);
+        GlobalSettings.musicPlayer.setOnErrorListener((mp, what, extra) -> {
             mp.release();
             return false;
         });
 //        新增錯誤監聽器
 //        add the error listener
-        GlobalSettings.mediaPlayer.stop();
-        return prepareMediaPlayer();
-    }
-
-    protected boolean prepareMediaPlayer(){
-        try{
-            GlobalSettings.mediaPlayer.prepare();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
+        if (playControl) {
+            GlobalSettings.playMusic();
         }
-        return true;
     }
-//    移動狀態至prepared
-//    move the state to "prepared"
-    @Override
-    public void onBackPressed() {
-        startActivity(new Intent(GlobalSettings.this,Title.class));
-        finish();
+
+    public static void playMusic() {
+        GlobalSettings.musicPlayer.start();
     }
+
+    public static void stopMusic() {
+        GlobalSettings.musicPlayer.stop();
+        GlobalSettings.musicPlayer.prepareAsync();
+        GlobalSettings.musicPlayer.setOnPreparedListener(mp -> {
+        });
+    }
+
+    public static void playMusic(boolean playControl) {
+        if (playControl) {
+            GlobalSettings.playMusic();
+        } else {
+            GlobalSettings.stopMusic();
+        }
+    }
+
+    public static void pauseMusic() {
+        if (GlobalSettings.musicControl) {
+            GlobalSettings.musicPlayer.pause();
+        }
+    }
+
 }
